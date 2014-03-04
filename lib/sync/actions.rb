1a7d858f791c7537e067dea3d82b9555fe1def6a
Alex Dubovskoy <dubovskoy.a@gmail.com>
2014-03-04 20:53:27 +0400

    next

--- lib/sync/actions.rb
+++ lib/sync/actions.rb
@@ -10,7 +10,8 @@ module Sync
     #   scope - The ActiveModel resource to scope update channel to
     #
     def sync_update(resource, options = {})
-      sync resource, :update, options
+      p "------sync_update----> #{resource}"
+      # sync resource, :update, options
     end
 
     # Publish destroy event to pubsub server for given resource
@@ -18,21 +19,23 @@ module Sync
     # resource - The ActiveModel resource, or Array of ActiveModel resources
     # options - The Hash of options
     #   scope - The ActiveModel resource to scope destroy channel to
-    #   
+    #
     def sync_destroy(resource, options = {})
-      sync resource, :destroy, options
+      p "------sync_destroy----> #{resource}"
+      # sync resource, :destroy, options
     end
 
-    # Render all sync'd partials for resource to string and publish 
+    # Render all sync'd partials for resource to string and publish
     # new action to pubsub server with rendered resource messages
     #
     # resource - The ActiveModel resource, or Array of ActiveModel resources
     # action - The Symbol action to publish. One of :update, :destroy
     # options - The Hash of options
     #   scope - The ActiveModel resource to scope destroy channel to
-    #   
+    #
     def sync_new(resource, options = {})
-      sync_new resource, options
+      p "------sync_new----> #{resource}"
+      # sync_new resource, options
     end
 
     # Render all resource sync'd partial to string and publish action
@@ -42,7 +45,7 @@ module Sync
     # action - The Symbol action to publish. One of :update, :destroy
     # options - The Hash of options
     #   scope - The ActiveModel resource to scope destroy channel to
-    #   
+    #
     def sync(resource, action, options = {})
       resources = [resource].flatten
       messages = resources.collect do |resource|
@@ -54,24 +57,25 @@ module Sync
       Sync.client.batch_publish(messages.flatten)
     end
 
-    # Render all sync'd partials for resource to string and publish 
+    # Render all sync'd partials for resource to string and publish
     # new action to pubsub server with rendered resource messages
     #
     # resource - The ActiveModel resource, or Array of ActiveModel resources
     # action - The Symbol action to publish. One of :update, :destroy
     # options - The Hash of options
     #   scope - The ActiveModel resource to scope destroy channel to
-    #   
+    #
     def sync_new(resource, options = {})
       scope = options[:scope]
-      resources = [resource].flatten
-      messages = resources.collect do |resource|
-        all_partials(resource, sync_render_context).collect do |partial|
-          partial.creator_for_scope(scope).message
-        end
-      end
+      p "------sync_new----> #{resource.inspect}"
+      # resources = [resource].flatten
+      # messages = resources.collect do |resource|
+      #   all_partials(resource, sync_render_context).collect do |partial|
+      #     partial.creator_for_scope(scope).message
+      #   end
+      # end
 
-      Sync.client.batch_publish(messages.flatten)
+      # Sync.client.batch_publish(messages.flatten)
     end
 
 
@@ -79,7 +83,7 @@ module Sync
 
     # The Context class handling partial rendering
     def sync_render_context
-      @sync_render_context ||= Renderer.new
+      # @sync_render_context ||= Renderer.new
     end
 
     # Returns Array of Partials for all given resource and context, including
--- lib/sync/model.rb
+++ lib/sync/model.rb
@@ -39,13 +39,13 @@ module Sync
         actions.flatten!
 
         if actions.include? :create
-          after_commit :publish_sync_create, :on => :create, :if => lamda { Sync::Model.enabled? }
+          after_create :publish_sync_create, :on => :create#, :if => lamda { Sync::Model.enabled? }
         end
         if actions.include? :update
-          after_commit :publish_sync_update, :on => :update, :if => lamda { Sync::Model.enabled? }
+          after_update :publish_sync_update, :on => :update#, :if => lamda { Sync::Model.enabled? }
         end
         if actions.include? :destroy
-          after_commit :publish_sync_destroy, :on => :destroy, :if => lamda { Sync::Model.enabled? }
+          after_destroy :publish_sync_destroy, :on => :destroy#, :if => lamda { Sync::Model.enabled? }
         end
       end
     end
@@ -60,22 +60,40 @@ module Sync
         Sync::Model.context || super
       end
 
-      def publish_sync_create        
-        sync_new self, :scope => sync_scope
-        sync_update sync_scope.reload if sync_scope
+      def publish_sync_create
+        p "-----------publish_sync_create"
+        p Sync.config
+        # sync_new self, :scope => sync_scope
+        # sync_update sync_scope.reload if sync_scope
       end
 
-      def publish_sync_update        
-        if sync_scope
-          sync_update [self, sync_scope.reload]
-        else
-          sync_update self
+      def publish_sync_update
+        p "-----------publish_sync_update"
+        p self.id
+        uri = URI("http://call.gettable.dev:3000/bookings/25243/bumper")
+        p uri
+        req = Net::HTTP::Put.new(uri.request_uri)
+
+        req["Content-Length"] = '0'
+        req["Content-Type"] = 'text/plain;charset=UTF-8'
+
+        res = Net::HTTP.start(uri.host, uri.port) do
+          |http| http.request(req)
         end
+
+        p res
+        # if sync_scope
+        #   sync_update [self, sync_scope.reload]
+        # else
+        #   sync_update self
+        # end
       end
 
-      def publish_sync_destroy        
-        sync_destroy self
-        sync_update sync_scope.reload if sync_scope
+      def publish_sync_destroy
+        p "-----------publish_sync_destroy"
+        p Sync.config
+        # sync_destroy self
+        # sync_update sync_scope.reload if sync_scope
       end
     end
   end
